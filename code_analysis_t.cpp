28c15ae09f419bba92c4824ba2342c4313dfe4ef
Steven Tyler Bissell <stb49@zips.uakron.edu>
2018-04-04 16:21:29 -0400

    given_url and option_url failed test

--- code_analysis_t.cpp
+++ code_analysis_t.cpp
@@ -96,5 +96,22 @@ int main() {
         assert(code_analysis(request) == false);
     }
 
+    // given_url and option_url filled in
+    {
+        analysis_request request;
+        request.given_filename  = "";
+        request.entry_filename  = "";
+        request.given_url       = "given_url";
+        request.option_filename = "";
+        request.option_url      = "option_url";
+        request.option_language = "";
+
+        auto filename = analysis_filename(request);
+        assert(filename == "");
+        assert(analysis_url(request) == "option_url");
+        assert(analysis_language(request, filename) == "");
+        assert(code_analysis(request) == false);
+    }
+
     return 0;
 }
