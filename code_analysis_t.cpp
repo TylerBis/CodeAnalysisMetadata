2c79d3f3d31a7446f8e2e00a259f9a4673274227
Steven Tyler Bissell <stb49@zips.uakron.edu>
2018-04-04 16:02:10 -0400

    test for source archive

--- code_analysis_t.cpp
+++ code_analysis_t.cpp
@@ -44,5 +44,21 @@ int main() {
         assert(code_analysis(request) == false);
     }
 
+    {
+        analysis_request request;
+        request.given_filename  = "given_filename.cpp";
+        request.entry_filename  = "entry_filename";
+        request.given_url       = "";
+        request.option_filename = "";
+        request.option_url      = "";
+        request.option_language = "";
+
+        auto filename = analysis_filename(request);
+        assert(filename == "");
+        assert(analysis_url(request) == "");
+        assert(analysis_language(request, filename) == "");
+        assert(code_analysis(request) == false);
+    }
+
     return 0;
 }
