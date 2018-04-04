42cb9c8fdb3781d51dc38babbf1e43970996f206
Steven Tyler Bissell <stb49@zips.uakron.edu>
2018-04-04 16:27:09 -0400

    option_language test fail

--- code_analysis_t.cpp
+++ code_analysis_t.cpp
@@ -113,5 +113,22 @@ int main() {
         assert(code_analysis(request) == false);
     }
 
+    // option_language filled in
+    {
+        analysis_request request;
+        request.given_filename  = "";
+        request.entry_filename  = "";
+        request.given_url       = "";
+        request.option_filename = "";
+        request.option_url      = "";
+        request.option_language = "option_language";
+
+        auto filename = analysis_filename(request);
+        assert(filename == "");
+        assert(analysis_url(request) == "");
+        assert(analysis_language(request, filename) == "option_language");
+        assert(code_analysis(request) == false);
+    }
+
     return 0;
 }
